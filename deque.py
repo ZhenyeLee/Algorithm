c84493cf7d7da63fd9bc0f6c6448cc88acff441c
ZhenyeLee <73271404+ZhenyeLee@users.noreply.github.com>
2020-12-19 22:16:37 -0600

    Add files via upload

---
+++ deque.py
@@ -0,0 +1,31 @@
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+from collections import deque
+search_queue = deque()
+search_queue += graph["you"]
+searched = []
+def search(name):
+ search_queue = deque()
+ search_queue+= graph[name]
+while search_queue:                                                                                                                                     
+    person = search_queue.popleft()
+    if not person in searched:  
+        if person_is_seller(person):
+            print(person+" is the person you want")
+        else:
+            search_queue += graph[person]
+            searched.append(person)
+
+
+print(search('you'))
